e6193ea6cb09f70218d92840fc4b1ae1ae0c82ca
predo17 <150456318+predo17@users.noreply.github.com>
2024-03-25 10:14:35 -0300

    controle de
    academia

---
+++ instructors.js
@@ -0,0 +1,23 @@
+const fs = require ("fs")
+const data = require ("./data.json")
+
+exports.post = function(req,res){
+   
+    const keys = Object.keys (req.body)
+
+    for(key of keys){
+        if(req.body[key]== ""){
+        return res.send ("Responda todo os dados")
+        }
+   
+    }
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json",JSON.stringify(data,null,2),function(err){
+        if(err) return res.send ("Erro ao salvar o arquivo")
+        return res.redirect("/instructos")
+    })
+
+    return res.send (req.body)
+    
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require ('express')
+const server = express ()
+const nunjucks = require ('nunjucks')
+const routes = require ("./routes");
+
+
+server.use (express.urlencoded({extended: true}));
+server.use (express.static('public'));
+server.use (routes);
+
+
+server.set ("view engine","njk")
+
+nunjucks.configure("views",{
+    express:server,
+    autoescape:false,
+    noCache:true
+
+})
+
+server.listen(5000,function(){
+    (console.log ("server is runing"))
+})
+

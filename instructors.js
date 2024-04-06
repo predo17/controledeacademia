a5cc8043906e72babbb0288597f92825b5e1e935
predo17 <150456318+predo17@users.noreply.github.com>
2024-04-06 12:02:04 -0300

    controle de
    academia

--- instructors.js
+++ instructors.js
@@ -1,6 +1,18 @@
 const fs = require ("fs")
 const data = require ("./data.json")
+const { name } = require("browser-sync")
+exports.show = function(req,res){
+    const {id} =req.params
 
+    const foundInstructor = data.instructors.find (function(instructor){
+        return instructor.id == id
+    })
+    if (!foundInstructor) return res.send("Instructor não encontrado")
+
+    return res.render ("instructors/show",{instructor:foundInstructor})
+
+
+}
 exports.post = function(req,res){
    
     const keys = Object.keys (req.body)
@@ -9,17 +21,18 @@ exports.post = function(req,res){
         if(req.body[key]== ""){
         return res.send ("Responda todo os dados")
         }
-        
-   
     }
-    data.instructors.push(req.body)
+    let {avatar_URL,birth,gender,service,created_at} = req.body
+
+    birth = Date.parse (req.body.birth)
+    created_at = Date.now()
+    id = Number (data.instructors.length)+1
+
+    data.instructors.push({id,avatar_URL,birth,gender,service,created_at})
+
 
     fs.writeFile("data.json",JSON.stringify(data,null,2),function(err){
         if(err) return res.send ("Erro ao salvar o arquivo")
-        return res.redirect("/instructos")
-        
+        return res.redirect("/instructors")
     })
-
-    return res.send (req.body)
-  
 }
